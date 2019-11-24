070ab44bf461d938c10aa7503c4eee43d07aedd3
unknown <hannahuseva@gmail.com>
2019-11-24 16:36:47 -0600

    Minor link corrections

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,10 +3,10 @@ import React from "react";
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
+      <a className="navbar-brand" href="prof_portfolio/">
         Main
       </a>
-      <a className="navbar-brand" href="/projects">
+      <a className="navbar-brand" href="prof_portfolio/projects">
         Projects
       </a>
       <p className="nav navbar-nav" style={{paddingLeft: "35%"}}>HANNA HUSEVA </p>
