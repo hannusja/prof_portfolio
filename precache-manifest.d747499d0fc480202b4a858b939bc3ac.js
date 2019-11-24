61afa7ce8ea4b7b94379aa5accb96e76a44caf88
unknown <hannahuseva@gmail.com>
2019-11-24 16:28:14 -0600

    Updates

---
+++ precache-manifest.d747499d0fc480202b4a858b939bc3ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c5a66c2c68027e0d01ee350d321db8",
+    "url": "/prof_portfolio/index.html"
+  },
+  {
+    "revision": "e8d6a7662a4190ffa71f",
+    "url": "/prof_portfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "84307a049f8a640aac76",
+    "url": "/prof_portfolio/static/js/2.04bcb57f.chunk.js"
+  },
+  {
+    "revision": "e8d6a7662a4190ffa71f",
+    "url": "/prof_portfolio/static/js/main.cce66d3d.chunk.js"
+  },
+  {
+    "revision": "7ce3a7d56a1aee77d291",
+    "url": "/prof_portfolio/static/js/runtime-main.96507c53.js"
+  }
+]);
