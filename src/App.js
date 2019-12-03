29fc928017f1d38f1ebd210c1fef7fdea97c51ba
unknown <hannahuseva@gmail.com>
2019-12-02 20:37:46 -0600

    Solved router gh-pages deploy issue

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "./pages/Main";
 import Projects from "./pages/Projects"
 import Nav from "./components/Nav";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename="/">
       <div>
         <Nav />
         <Switch>
           <Route exact path="/" component={Main} />
-          <Route exact path="/projects" component={Projects} />
+          <Route path="/projects" component={Projects} />
         </Switch>
       </div>
     </Router>
